5ddd10917381456486e418f1868767f6f61be1f9
Samuel De Backer <sdebacker@gmail.com>
2020-03-06 17:15:02 +0100

    Update edit.blade.php

--- src/resources/views/public/edit.blade.php
+++ src/resources/views/public/edit.blade.php
@@ -9,35 +9,41 @@
 
     @include('subscriptions::public._alerts')
 
+    <h1>@lang('Edit your profile')</h1>
+
     {!! BootForm::open() !!}
     {!! BootForm::bind(auth()->user()) !!}
-    <div class="row">
-        <div class="col">
-            {!! BootForm::text(__('First name'), 'first_name')->required() !!}
-        </div>
-        <div class="col">
-            {!! BootForm::text(__('Last name'), 'last_name')->required() !!}
+    <div class="card bg-light">
+        <div class="card-body">
+            <div class="row">
+                <div class="col">
+                    {!! BootForm::text(__('First name'), 'first_name')->required() !!}
+                </div>
+                <div class="col">
+                    {!! BootForm::text(__('Last name'), 'last_name')->required() !!}
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    {!! BootForm::text(__('Street'), 'street')->required() !!}
+                </div>
+                <div class="col-md-3">
+                    {!! BootForm::text(__('Number'), 'number')->required() !!}
+                </div>
+            </div>
+            {!! BootForm::text(__('City'), 'city')->required() !!}
+            <div class="row">
+                <div class="col">
+                    {!! BootForm::text(__('Zip'), 'zip')->required() !!}
+                </div>
+                <div class="col">
+                    {!! BootForm::text(__('Country'), 'country')->required() !!}
+                </div>
+            </div>
+            {!! BootForm::submit(__('Save')) !!}
+            <a class="btn btn-link text-secondary" href="{{ route($lang.'::subscriptions-profile') }}">@lang('Cancel')</a>
+            {!! BootForm::close() !!}
         </div>
     </div>
-    <div class="row">
-        <div class="col">
-            {!! BootForm::text(__('Street'), 'street')->required() !!}
-        </div>
-        <div class="col-md-3">
-            {!! BootForm::text(__('Number'), 'number')->required() !!}
-        </div>
-    </div>
-    {!! BootForm::text(__('City'), 'city')->required() !!}
-    <div class="row">
-        <div class="col">
-            {!! BootForm::text(__('Zip'), 'zip')->required() !!}
-        </div>
-        <div class="col">
-            {!! BootForm::text(__('Country'), 'country')->required() !!}
-        </div>
-    </div>
-    {!! BootForm::submit(__('Save')) !!}
-    <a class="btn btn-link text-secondary" href="{{ route($lang.'::subscriptions-profile') }}">@lang('Cancel')</a>
-    {!! BootForm::close() !!}
 
 @endsection
