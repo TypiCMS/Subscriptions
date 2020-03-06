7407ab708da7ab499f9d2a5d86c30f7ccd7232e0
Samuel De Backer <sdebacker@gmail.com>
2020-03-06 22:44:28 +0100

    Update permissions.php

--- src/config/permissions.php
+++ src/config/permissions.php
@@ -3,8 +3,6 @@
 return [
     'subscriptions' => [
         'see-all-subscriptions' => 'View list',
-        'create-subscription' => 'Create',
         'update-subscription' => 'Edit',
-        'delete-subscription' => 'Delete',
     ],
 ];
