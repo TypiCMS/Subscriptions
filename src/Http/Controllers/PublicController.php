ae30cabc08e1c062b29549e400af65c626812c65
Samuel De Backer <sdebacker@gmail.com>
2020-03-10 22:58:20 +0100

    CS

--- src/Http/Controllers/PublicController.php
+++ src/Http/Controllers/PublicController.php
@@ -59,7 +59,9 @@ class PublicController extends BasePublicController
     {
         Auth::user()->update($request->validated());
 
-        return redirect()->route(app()->getLocale().'::subscriptions-profile')->with('success', __('Your profile has been successfully updated.'));
+        return redirect()
+            ->route(app()->getLocale().'::subscriptions-profile')
+            ->with('success', __('Your profile has been successfully updated.'));
     }
 
     public function paymentMethodRevoke(Request $request, $id)
