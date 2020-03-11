24a587e6846adfa9f8a2bd961a7940e932f7095c
Samuel De Backer <sdebacker@gmail.com>
2020-03-11 22:46:31 +0100

    metadata added to user

--- src/Models/BillableUser.php
+++ src/Models/BillableUser.php
@@ -56,6 +56,18 @@ class BillableUser extends User
             'email' => $this->email,
             'name' => "$this->first_name $this->last_name",
             'locale' => $this->getLocale(),
+            'metadata' => $this->getMetadata(),
+        ];
+    }
+
+    public function getMetaData(): array
+    {
+        return [
+            'street' => $this->street,
+            'number' => $this->number,
+            'zip' => $this->zip,
+            'city' => $this->city,
+            'country' => $this->country,
         ];
     }
 
