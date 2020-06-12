a5bc8038ee8b053562554e68a92f0158b331163b
Samuel De Backer <sdebacker@gmail.com>
2020-06-12 08:48:52 +0200

    Update _plans-radio-buttons.blade.php

--- src/resources/views/public/_plans-radio-buttons.blade.php
+++ src/resources/views/public/_plans-radio-buttons.blade.php
@@ -4,9 +4,11 @@
         <div class="form-check">
             {!! Form::radio('plan', $name)->id('plan-'.$name)->addClass('form-check-input') !!}
             <label class="form-check-label" for="{{ 'plan-'.$name }}">
-                <span>@lang($name)</span><br>
-                <span class="text-muted small">
+                <span class="account-subscription-item-name">@lang($name)</span>
+                <span class="account-subscription-item-description">@lang($plan['description'])</span>
+                <span class="account-subscription-item-amount">
                     @lang(':amount / '.$plan['interval'], ['amount' => Subscriber::planPriceFormat($plan['amount']['value'], auth()->user()->taxPercentage(), $plan['amount']['currency'], auth()->user()->getLocale())])
+                </span>
             </label>
         </div>
     </li>
