0a6c32f587c72cb34e29f33aadbc9c6bad873fa5
Samuel De Backer <sdebacker@gmail.com>
2020-09-20 22:41:12 +0200

    Routes definition cleaned

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -6,71 +6,61 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use TypiCMS\Modules\Core\Facades\TypiCMS;
+use TypiCMS\Modules\Subscriptions\Http\Controllers\AdminController;
+use TypiCMS\Modules\Subscriptions\Http\Controllers\ApiController;
+use TypiCMS\Modules\Subscriptions\Http\Controllers\PublicController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to the controller routes in your routes file.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'TypiCMS\Modules\Subscriptions\Http\Controllers';
-
     /**
      * Define the routes for the application.
      */
     public function map()
     {
-        Route::namespace($this->namespace)->group(function (Router $router) {
+        foreach (locales() as $lang) {
+            Route::middleware('web')->get($lang.'/webhooks/cashier/check-payment/{payment_id}', [PublicController::class, 'checkPayment']);
+        }
+
+        /*
+         * Front office routes
+         */
+        if ($page = TypiCMS::getPageLinkedToModule('subscriptions')) {
+            $middleware = ['public', 'auth'];
             foreach (locales() as $lang) {
-                $router->middleware('web')->get($lang.'/webhooks/cashier/check-payment/{payment_id}', ['uses' => 'PublicController@checkPayment']);
+                if ($page->isPublished($lang) && $uri = $page->uri($lang)) {
+                    Route::middleware($middleware)->prefix($uri)->name($lang.'::')->group(function (Router $router) {
+                        $router->get('/', [PublicController::class, 'profileIndex'])->name('subscriptions-profile');
+                        $router->get('edit', [PublicController::class, 'profileEdit'])->name('subscriptions-profile-edit');
+                        $router->post('edit', [PublicController::class, 'profileUpdate'])->name('subscriptions-profile-update');
+                        $router->get('payment-method/{id}', [PublicController::class, 'paymentMethodRevoke'])->name('subscriptions-paymentmethod-revoke');
+                        $router->post('payment-method', [PublicController::class, 'paymentMethodUpdate'])->name('subscriptions-paymentmethod-update');
+                        $router->get('plans', [PublicController::class, 'plans'])->name('subscriptions-plans');
+                        $router->post('plans', [PublicController::class, 'subscribe'])->name('subscriptions-subscribe');
+                        $router->post('plans/upgrade', [PublicController::class, 'upgrade'])->name('subscriptions-upgrade');
+                        $router->get('plans/cancel', [PublicController::class, 'cancel'])->name('subscriptions-cancel');
+                        $router->get('plans/resume', [PublicController::class, 'resume'])->name('subscriptions-resume');
+                        $router->get('invoice/{number}', [PublicController::class, 'invoice'])->name('subscriptions-invoice');
+                        $router->get('invoice/{number}/download', [PublicController::class, 'downloadInvoice'])->name('subscriptions-download-invoice');
+                    });
+                }
             }
+        }
 
-            /*
-             * Front office routes
-             */
-            if ($page = TypiCMS::getPageLinkedToModule('subscriptions')) {
-                $router->middleware('public')->group(function (Router $router) use ($page) {
-                    $options = ['middleware' => 'auth'];
-                    foreach (locales() as $lang) {
-                        if ($page->translate('status', $lang) && $uri = $page->uri($lang)) {
-                            $router->get($uri, $options + ['uses' => 'PublicController@profileIndex'])->name($lang.'::subscriptions-profile');
-                            $router->get($uri.'/edit', $options + ['uses' => 'PublicController@profileEdit'])->name($lang.'::subscriptions-profile-edit');
-                            $router->post($uri.'/edit', $options + ['uses' => 'PublicController@profileUpdate'])->name($lang.'::subscriptions-profile-update');
-                            $router->get($uri.'/payment-method/{id}', $options + ['uses' => 'PublicController@paymentMethodRevoke'])->name($lang.'::subscriptions-paymentmethod-revoke');
-                            $router->post($uri.'/payment-method', $options + ['uses' => 'PublicController@paymentMethodUpdate'])->name($lang.'::subscriptions-paymentmethod-update');
-                            $router->get($uri.'/plans', $options + ['uses' => 'PublicController@plans'])->name($lang.'::subscriptions-plans');
-                            $router->post($uri.'/plans', $options + ['uses' => 'PublicController@subscribe'])->name($lang.'::subscriptions-subscribe');
-                            $router->post($uri.'/plans/upgrade', $options + ['uses' => 'PublicController@upgrade'])->name($lang.'::subscriptions-upgrade');
-                            $router->get($uri.'/plans/cancel', $options + ['uses' => 'PublicController@cancel'])->name($lang.'::subscriptions-cancel');
-                            $router->get($uri.'/plans/resume', $options + ['uses' => 'PublicController@resume'])->name($lang.'::subscriptions-resume');
-                            $router->get($uri.'/invoice/{number}', $options + ['uses' => 'PublicController@invoice'])->name($lang.'::subscriptions-invoice');
-                            $router->get($uri.'/invoice/{number}/download', $options + ['uses' => 'PublicController@downloadInvoice'])->name($lang.'::subscriptions-download-invoice');
-                        }
-                    }
-                });
-            }
+        /*
+         * Admin routes
+         */
+        Route::middleware('admin')->prefix('admin')->name('admin::')->group(function (Router $router) {
+            $router->get('subscriptions', [AdminController::class, 'index'])->name('index-subscriptions')->middleware('can:read subscriptions');
+            $router->get('subscriptions/{subscription}', [AdminController::class, 'show'])->name('show-subscription')->middleware('can:update-subscription');
+            $router->post('subscriptions/{subscription}/cancel', [AdminController::class, 'cancel'])->name('cancel-subscription')->middleware('can:update-subscription');
+            $router->post('subscriptions/{subscription}/resume', [AdminController::class, 'resume'])->name('resume-subscription')->middleware('can:update-subscription');
+        });
 
-            /*
-             * Admin routes
-             */
-            $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('subscriptions', 'AdminController@index')->name('admin::index-subscriptions')->middleware('can:see-all-subscriptions');
-                $router->get('subscriptions/{subscription}', 'AdminController@show')->name('admin::show-subscription')->middleware('can:update-subscription');
-                $router->post('subscriptions/{subscription}/cancel', 'AdminController@cancel')->name('admin::cancel-subscription')->middleware('can:update-subscription');
-                $router->post('subscriptions/{subscription}/resume', 'AdminController@resume')->name('admin::resume-subscription')->middleware('can:update-subscription');
-            });
-
-            /*
-             * API routes
-             */
-            $router->middleware('api')->prefix('api')->group(function (Router $router) {
-                $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('subscriptions', 'ApiController@index')->middleware('can:see-all-subscriptions');
-                });
-            });
+        /*
+         * API routes
+         */
+        Route::middleware(['api', 'auth:api'])->prefix('api')->group(function (Router $router) {
+            $router->get('subscriptions', [ApiController::class, 'index'])->middleware('can:read subscriptions');
         });
     }
 }
