31cd80295773564e18135a70a22dfb0e54b9056a
Samuel De Backer <sdebacker@gmail.com>
2022-09-15 16:05:47 +0200

    _document-list & _image-list partials

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -17,8 +17,8 @@
     <div class="account">
 
         <div class="rich-content">{!! $page->present()->body !!}</div>
-        @include('files::public._documents', ['model' => $page])
-        @include('files::public._images', ['model' => $page])
+        @include('files::public._document-list', ['model' => $page])
+        @include('files::public._image-list', ['model' => $page])
 
         @include('subscriptions::public._alerts')
 
